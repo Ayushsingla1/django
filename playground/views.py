ae71f3f845b680769e842afd933457d46a29d923
Ayush singla <143862828+Ayushsingla1@users.noreply.github.com>
2026-02-10 23:15:16 +0530

    django-postgres

---
+++ playground/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from store.models import Product, ProductOrderMapping
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q, F
+# Create your views here.
+# here we write requestHandler
+
+def say_hello(request):
+
+    product = Product.objects.filter(pk = 0).first()
+    print(product)
+
+    # try:
+    #   product = Product.objects.get(pk = 0) 
+    # except ObjectDoesNotExist:
+    #   pass
+
+
+    # to get products where inventory < 20 | price < 10
+    # product = Product.objects.filter(Q(inventory__lt = 20) | Q(price__lt = 10))
+
+    # to get product where inventory = price
+    # product = Product.objects.filter(inventory = F('price'))
+
+    # query = Product.objects.values('id', 'title', 'collection__title')
+
+    # query = Product.objects.filter(id__in = ProductOrderMapping.objects.values('products_id').distinct()).order_by('title')
+    return render(request, "index.html")
---
+++ store/models.py
@@ -0,0 +1,71 @@
+from django.db import models
+
+# Create your models here.
+#
+
+
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    last_updated = models.DateTimeField(auto_now=True)
+    collections = models.ForeignKey(Collection, on_delete=models.PROTECT)
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(unique=True)
+    phone = models.CharField(max_length=10)
+    birth_date = models.DateField(null=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['email'])
+        ]
+
+
+class Order(models.Model):
+    ORDER_PAYMENT_STATUS = [("P", "Pending"), ("C", "Completed"), ("F", "Failed")]
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(max_length=1, choices=ORDER_PAYMENT_STATUS)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    zip = models.CharField(max_length=255)
+    customer = models.OneToOneField(
+        Customer, primary_key=True, on_delete=models.CASCADE
+    )  # one to one relationship
+    # customer = models.ForeignKey(
+    #     Customer, on_delete=models.CASCADE
+    # ) # one to many relationship
+    class Meta:
+        indexes = [
+            models.Index(fields=['city'])
+        ]
+
+
+class ProductOrderMapping(models.Model):
+    orders = models.ForeignKey(Order, on_delete=models.PROTECT)
+    products = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class CartItems(models.Model):
+    item = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
