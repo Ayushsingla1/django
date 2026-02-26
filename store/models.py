ef40f1c70bb7fbd7e0cfa8e85757c9342fa51597
Ayush singla <143862828+Ayushsingla1@users.noreply.github.com>
2026-02-27 01:42:43 +0530

    cart

--- store/models.py
+++ store/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from uuid import uuid4
 # Create your models here.
 #
 
@@ -82,17 +82,20 @@ class ProductOrderMapping(models.Model):
 
 
 class Cart(models.Model):
+    id = models.UUIDField(primary_key=True, default = uuid4)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
 class CartItem(models.Model):
-    item = models.ForeignKey(Product, on_delete=models.CASCADE)
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
     quantity = models.PositiveSmallIntegerField()
+
+    class Meta:
+        unique_together = [['cart','product']]
 
 
 class Review(models.Model):
-    
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
     name = models.CharField(max_length=255)
     review = models.TextField(max_length=255)
--- store/views.py
+++ store/views.py
@@ -9,7 +9,9 @@ from rest_framework.views import APIView
 # from rest_framework.mixins import CreateModelMixin, ListModelMixin
 from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.pagination import PageNumberPagination
+from .pagination import DefaultPagination
 # from django.db.models import FieldDoesNotExist
 # Create your views here.
 
@@ -17,8 +19,11 @@ class ProductViewSet(ModelViewSet):
 
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ['collections_id']
+    search_fields = ['title','description']
+    ordering_fields = ['price','last_updated']
+    pagination_class = DefaultPagination
 
     # def get_queryset(self):
     #     queryset = Product.objects.all()
